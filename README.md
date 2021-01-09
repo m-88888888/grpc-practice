38b63b3a9cb7d1d2c3d37f3e8466108d9c790055
m-88888888 <mochi.h8888@gmail.com>
2021-01-09 22:12:34 +0900

    2つの数値を合計して返すgRPCサーバー作成

--- README.md
+++ README.md
@@ -3,3 +3,27 @@
 - grpcサーバー作成する
   - evansからリクエスト投げる
 - gorm使ってリクエスト→DB操作をやってみる
+
+## protocコマンド
+
+```sh
+protoc --go_out=./pb/calc --go_opt=paths=source_relative \
+--go-grpc_out=./pb/calc --go-grpc_opt=paths=source_relative \
+proto/calc.proto
+```
+
+--go_out
+calc.pb.goを生成。
+Request, ResponseのMessage Typeを含む。
+
+--go-grpc_out
+calc_grpc.pb.goを生成。
+client, serverのコードを含む。
+
+## evans
+
+```sh
+evans repl --proto proto/calc.proto
+# replモード
+call Sum
+```
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/m-88888888/grpc-practice/pb/calc/proto"
+
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+const port = ":50051"
+
+type ServerUnary struct {
+	pb.UnimplementedCalcServer
+}
+
+func (s *ServerUnary) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumReply, error) {
+	// Requestからmessageを取得
+	a := in.GetA()
+	b := in.GetB()
+	fmt.Println(a, b)
+	reply := fmt.Sprintf("%d + %d = %d", a, b, a+b)
+	return &pb.SumReply{
+		Message: reply,
+	}, nil
+}
+
+func server() error {
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		return errors.Wrap(err, "missing port.")
+	}
+	s := grpc.NewServer()
+	var server ServerUnary
+	pb.RegisterCalcServer(s, &server)
+	if err := s.Serve(listen); err != nil {
+		return errors.Wrap(err, "Failed to start server.")
+	}
+	return nil
+}
 
 func main() {
-	fmt.Printf("Hello, World!")
+	fmt.Printf("gRPC server start.")
+	if err := server(); err != nil {
+		log.Fatalf("%v", err)
+	}
 }
